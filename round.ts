6ed8fa651e3f6928d91dd45c1e71e9936ca75bda
Connor-Wilkins <159037610+SchoolAccount2123@users.noreply.github.com>
2025-11-19 19:57:58 +0000

    almost finished code created the function

---
+++ round.ts
@@ -0,0 +1,20 @@
+//function rounds a number and formats it to a fixed field size
+
+function formatnumb(num: number, decimals: number, fieldsize: number): string {
+  const rounded = num.toFixed(decimals);
+  return rounded.padStart(fieldsize, " ");}
+
+
+// The given numbers
+const n1 = 8.5467;
+const n2 = 9.6382;
+const n3 = 18.5146;
+const n4 = 125.496;
+
+//formatting the numbers
+const f1 = formatnumb(n1, 3, 10);
+const f2 = formatnumb(n2, 5, 8)
+// Display the results
+
+console.log("The rounded and formatted values are:")
+console.log(f1);
